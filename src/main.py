cd3862b1c038f418b13f6d410d743fd4b3bed11b
Sam Barati Bakhtiiari <sam_barati12@hotmail.com>
2026-04-27 14:35:37 +0200

    Remove redundant files

--- src/main.py
+++
@@ -1,7 +0,0 @@
-#
-#   To-Do:
-#
-#   User gives input (previous books (both ratings, genres) + some input such as genres (or maybe minimum rating))
-#   build query
-#   then query Elasticsearch (using search.py)
-#   return recommendations (using recommendations.py)
